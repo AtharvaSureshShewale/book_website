2e16ed0cbbd5a5b4e7aeff0b0040fd2cf95f708e
AtharvaSureshShewale <119092262+AtharvaSureshShewale@users.noreply.github.com>
2024-12-27 20:47:14 +0530

    add

---
+++ Final Book Website/Template/review.php
@@ -0,0 +1,121 @@
+<!-- Review Section -->
+
+<section class="reviews" id="reviews">
+    <h1 class="heading"><span>Client's Review</span></h1>
+
+    <div class="swiper reviews-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide box">
+                <img src="./assets/image/review_5.webp" alt="image">
+                <h3>Shashank Shewale</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Distinctio, adipisci iste? Voluptatum perspiciatis vero quas similique. 
+                    Modi est ut laborum.</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="./assets/image/review_2.png" alt="image">
+                <h3>Alice Fernades</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Distinctio, adipisci iste? Voluptatum perspiciatis vero quas similique. 
+                    Modi est ut laborum.</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="./assets/image/review_1.png" alt="image">
+                <h3>John Deo</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Distinctio, adipisci iste? Voluptatum perspiciatis vero quas similique. 
+                    Modi est ut laborum.</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="./assets/image/review_4.png" alt="image">
+                <h3>Mary D'souza</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Distinctio, adipisci iste? Voluptatum perspiciatis vero quas similique. 
+                    Modi est ut laborum.</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="./assets/image/review_3.png" alt="image">
+                <h3>John Bright</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Distinctio, adipisci iste? Voluptatum perspiciatis vero quas similique. 
+                    Modi est ut laborum.</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="./assets/image/review_6.jpg" alt="image">
+                <h3>Tanay Rambhia</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Distinctio, adipisci iste? Voluptatum perspiciatis vero quas similique. 
+                    Modi est ut laborum.</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="./assets/image/review_7.jpg" alt="image">
+                <h3>Sujal Jain</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                    Distinctio, adipisci iste? Voluptatum perspiciatis vero quas similique. 
+                    Modi est ut laborum.</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<!-- !Review Section -->
